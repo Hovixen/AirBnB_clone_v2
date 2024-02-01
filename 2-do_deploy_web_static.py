09c89a7b7e01bcb59a51fcc1675a51f84198fb1f
Hovixen <orevaerijo@gmail.com>
2024-02-01 18:57:50 +0000

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,48 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
-from datetime import datetime
+"""
+Script distributes an archive to web servers
+"""
 from fabric.api import *
-import shlex
 import os
 
-
-env.hosts = ['35.231.33.237', '34.74.155.163']
-env.user = "ubuntu"
+env.user = 'ubuntu'
+env.hosts = ['100.24.244.104', '3.89.155.116']
+env.key_filename = ['~/.ssh/id_rsa']
 
 
 def do_deploy(archive_path):
-    """ Deploys """
+    """
+    Distribute an archive to web servers and perform deployment steps.
+
+    Args:
+        archive_path (str): Path to the archive to be deployed.
+
+    Returns:
+        bool: True if all operations were successful, False otherwise.
+    """
+
     if not os.path.exists(archive_path):
+        print("Error: {} not found".format(archive_path))
         return False
+
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
+        put(archive_path, '/tmp/')
 
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
+        ar_file = archive_path.split('/')[-1]
 
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
+        release_folder = '/data/web_static/releases/{}'.format(
+                            ar_file.split(".")[0])
 
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+        run("mkdir -p {}".format(release_folder))
+        run("tar -xzf /tmp/{} -C {}".format(ar_file, release_folder))
+        run("rm /tmp/{}".format(ar_file))
+        run("mv {}/web_static/* {}".format(release_folder, release_folder))
+        run("rm -rf {}/web_static".format(release_folder))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        run("ln -s {} /data/web_static/current".format(release_folder))
+
+        print("New Version deployed!")
         return True
-    except:
+    except Exception as e:
+        print("Error while deploying: {}".format(e))
         return False
